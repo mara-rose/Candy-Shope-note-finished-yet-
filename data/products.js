eba60daf614f4953a11c1159e31d3df7ea4c77cf
marambelhannachi <maramexx3@gmail.com>
2025-11-13 17:00:09 +0100

    Use products js file and generate html

---
+++ data/products.js
@@ -0,0 +1,154 @@
+const products =[
+ { image: 'images/candy/haribo.webp',
+  name : 'Haribo GoldBears',
+  rating :{
+    stars : 4.5,
+    count : 87
+  },
+  price : 870
+ },
+  { image: 'images/candy/motts.avif',
+  name : 'Motts Fruits',
+  rating :{
+    stars : 4,
+    count : 127
+  },
+  price : 1200
+ },
+   { image: 'images/candy/life-savers.avif',
+  name : 'Life Savers (Mintes)',
+  rating :{
+    stars : 3.5,
+    count : 300
+  },
+  price : 999.99
+ },
+   { image: 'images/candy/white-chocolate.avif',
+  name : 'Ghirardelle - White Chocolate',
+  rating :{
+    stars : 4.5,
+    count : 20
+  },
+  price : 980
+ },
+    { image: 'images/candy/jolly.avif',
+  name : 'Jolly Rancher Gummies',
+  rating :{
+    stars : 2.5,
+    count : 56
+  },
+  price : 450
+ },
+  { image: 'images/candy/snicker-s.avif',
+  name : 'Chocolate Snicker',
+  rating :{
+    stars : 0,
+    count : 1010
+  },
+  price : 500
+ },
+   { image: 'images/candy/kit-kat.avif',
+  name : 'Chocolate Kit-Kat',
+  rating :{
+    stars : 3.5,
+    count : 100
+  },
+  price : 500
+ },
+    { image: 'images/candy/ferrero.avif',
+  name : 'Ferrero Snick',
+  rating :{
+    stars : 3,
+    count : 40
+  },
+  price : 1500
+ },
+ { image: 'images/candy/lindt.webp',
+  name : 'Lindt',
+  rating :{
+    stars : 2.5,
+    count : 10
+  },
+  price : 770
+ },
+  { image: 'images/candy/ritz.avif',
+  name : 'Ritz Candy',
+  rating :{
+    stars : 4.5,
+    count : 99
+  },
+  price : 400
+ },
+  { image: 'images/candy/skittles.webp',
+  name : 'Skittles Sweets Colores',
+  rating :{
+    stars : 3.5,
+    count : 240
+  },
+  price : 250
+ },
+  { image: 'images/candy/reastables.avif',
+  name : 'Feastables Chocolate',
+  rating :{
+    stars : 5,
+    count : 850
+  },
+  price : 550
+ },
+  { image: 'images/candy/hi-chewavif.avif',
+  name : 'Hi-Chew Stawbarry Dessert',
+  rating :{
+    stars : 3.5,
+    count : 99
+  },
+  price : 699.99
+ },
+  { image: 'images/candy/hershey\'s symphony.avif',
+  name : 'Symphony Hershey\'s',
+  rating :{
+    stars : 3,
+    count : 2200
+  },
+  price : 750.5
+ },
+   { image: 'images/candy/trolli.avif',
+  name : 'Trolli Sour',
+  rating :{
+    stars : 1.5,
+    count : 35
+  },
+  price : 890
+ },
+  { image: 'images/candy/kinder-bureno.avif',
+  name : 'Kinder Bureno',
+  rating :{
+    stars : 4.5,
+    count : 20
+  },
+  price : 500
+ },
+  { image: 'images/candy/Almondy-Joy.avif',
+  name : 'Almondy Joy',
+  rating :{
+    stars : 3,
+    count : 77
+  },
+  price : 870
+ },
+  { image: 'images/candy/snickers.avif',
+  name : 'Snickers Minis',
+  rating :{
+    stars : 1.5,
+    count : 2100
+  },
+  price : 630.5
+ },
+  { image: 'images/candy/reese\'s-oreo.avif',
+  name : 'Reese Oreo',
+  rating :{
+    stars : 4,
+    count : 910
+  },
+  price : 810
+ },
+];
