b3f09b11f592bbfeec1da61f0fa5b16c8ab50484
marambelhannachi <maramexx3@gmail.com>
2025-11-18 16:33:42 +0100

    make the cart quantity interactive

--- scripts/CandyCloud.js
+++ scripts/CandyCloud.js
@@ -38,7 +38,7 @@ products.forEach((product) => {
           </div>
 
           <button class="add-to-cart-button button-primary js-add-to-cart "
-          data-product-name="${product.name}">
+          data-product-id="${product.id}">
           Add to Cart
           </button>
         </div>
@@ -50,10 +50,10 @@ document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
-    const productName = button.dataset.productName;
+    const productId = button.dataset.productId;
     let matchingItem;
     cart.forEach((item) => {
-      if (productName === item.productName) {
+      if (productId === item.productId) {
         matchingItem = item;
       }
     });
@@ -62,11 +62,15 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
       matchingItem.quantity += 1;
     } else {
       cart.push({
-        productName: productName,
+        productId: productId,
         quantity: 1,
       });
     }
 
-    console.log(cart);
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+    document.querySelector(".js-cart-quantity").innerText = cartQuantity;
   });
 });
