6727e6bd9b468d8678bf53c1a44a51762a9f056b
marambelhannachi <maramexx3@gmail.com>
2025-11-21 17:59:48 +0100

    add checkout page with styles

--- scripts/CandyCloud.js
+++ scripts/CandyCloud.js
@@ -1,7 +1,7 @@
 import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 
-let productsHTML = "";m
+let productsHTML = "";
 products.forEach((product) => {
   const html = `<div class="product-container">
           <div class="product-image-container">
