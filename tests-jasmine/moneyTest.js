ae16f4f2ee281b758a5e5c994927f24ee210fd93
marambelhannachi <maramexx3@gmail.com>
2026-05-11 17:24:32 +0100

    do test foramtCurrency with jasmine framwork

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,22 @@
+import formtCurrency
+ from "../scripts/utils/money.js";
+
+describe('test suite : formatCurrency',()=>{
+  it('converts cents into dollars',()=>{
+    expect(formtCurrency(2095)).toEqual('20.95');
+  });
+  
+  it('works with 0',()=>{
+    expect(formtCurrency(0)).toEqual('0.00');
+  });
+
+  it('rounds up to the nearest cent',()=>{
+    expect(formtCurrency(2000.5)).toEqual('20.01');
+  });
+
+  it('rounds down to the nearest cent',()=>{
+    expect(formtCurrency(2000.4)).toEqual('20.00');
+  })
+})
+
+
